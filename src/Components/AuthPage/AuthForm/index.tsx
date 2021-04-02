d062c7476a0669e6999c256e17468661cc79dfbf
headtop-maker <75481308+headtop-maker@users.noreply.github.com>
2021-04-02 17:51:54 +0300

    020421

---
+++ src/Components/AuthPage/AuthForm/index.tsx
@@ -0,0 +1,39 @@
+import React, { FC, useState } from "react";
+import PasswordInput from "../../Common/Forms/PasswordInput";
+import TextInput from "../../Common/Forms/TextInput";
+import style from "./AuthForm.module.scss";
+
+const AuthForm: FC = () => {
+  const [login, setLogin] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <>
+      <div className={style["auth-form_wrapper"]}>
+        <form className={style.form}>
+          <h1>Авторизация</h1>
+
+          {/* <TextInput setLogin={setLogin} login={login} title="Login" />
+          <PasswordInput
+            setPassword={setPassword}
+            password={password}
+            title="Password"
+          /> */}
+
+          <button
+            type="button"
+            className={style.btn}
+            onClick={() => {
+              console.log({ login, password }, "Объект авторизации");
+              setLogin("");
+              setPassword("");
+            }}>
+            LOGIN
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AuthForm;
